7d38ccb30b60e6ce0cd0bfbc6df520ce9256ad06
Himanshu Malik <himanshumalik0069@gmail.com>
2025-03-29 01:32:43 +0530

    added a Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
+import Button  from "@/components/Button"
 
 export default function Home() {
   return (
-              <div className="w-full h-screen flex items-center justify-center bg-gradient-to-b from-[#0D0D0D] to-[#001E1E]">
-                  <div className="text-center">
-                      <div className="text-6xl text-white"><h1>Welcome to Bruhs.in</h1></div>
-                      <div className="text-white/50 italic">Coming Soon.......</div>
-                      {/* <div className="text-white/50 italic">But you know we stay"   -The Bruhs</div> */}
-                      <div className="w-full m-2 flex justify-center space-x-4">
-                          {/* <Button>Hello</Button>
-                          <Button>Bruh</Button> */}
-                      </div>
-                  </div>
+    <div>
+      <div className="flex items-center h-screen p-20 place-content-between">
+        <div>
+          <div className="mb-8">
+            <h1 className="devoration-black text-8xl">Bruhs</h1>
+            <h2>The Group that doing but actully not doing anything.</h2>
+          </div>
+          <div className="flex space-x-4">
+            <Button>Contact Us</Button>
+            <Button>Read Articles</Button>
+          </div>
+        </div>
+        <div>
+          <img src="/av.png" alt=""/>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <h1 className="text-4xl">Bruh</h1>
+        <h2 className="text-3xl">Bruh</h2>
+        <h3 className="text-2xl">Bruh</h3>
+        <p className="text-xl">Bruh</p>
+      </div>
     </div>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
-    return(
-        <div className="w-full h-[80px] border-b-[2.5px] border-[#348888] drop-shadow-[0_5px_15px_#22BABB82] flex items-center justify-between px-6 bg-black text-white rounded-br-[20px] rounded-bl-[20px] fixed">
-            <Link className="text-4xl" href="/" >Bruh!</Link>
+    return (
+        <div className="w-full h-[60px] border-b-[2.5px] border-primaryDark drop-shadow-2xl flex items-center justify-between px-6 bg-primary text-white fixed">
+            <Link className="text-2xl flex items-center" style={{ height: "inherit"}} href="/">
+                <img src="/22.png" alt="" style={{ height: "inherit" }} />
+                Bruh!
+            </Link>
             <div className="flex space-x-6">
                 <Link href="/articles">Blog</Link>
                 <Link href="/contact">Contact</Link>
             </div>
         </div>
-    )
+    );
 }
