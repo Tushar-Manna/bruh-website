e3b0325f1c50225f1cb526b60557bedd12065e2f
Fusion Kun <fusion@FusionLap.FusionLap>
2025-03-29 09:21:05 +0530

    make some changes and It have a bug + the animetion icon is not showing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import Button  from "@/components/Button"
+import Button from "@/components/Button";
+import Lottie from "react-lottie-player";
+import animationData from "/Animation - 1743219469295.lottie";
 
 export default function Home() {
   return (
@@ -6,24 +8,30 @@ export default function Home() {
       <div className="flex items-center h-screen p-20 place-content-between">
         <div>
           <div className="mb-8">
-            <h1 className="devoration-black text-8xl">Bruhs</h1>
-            <h2>The Group that doing but actully not doing anything.</h2>
+            <h1 className="decoration-black text-8xl">Bruhs</h1>
+            <h2>The Group that is doing but actually not doing anything.</h2>
           </div>
           <div className="flex space-x-4">
             <Button>Contact Us</Button>
             <Button>Read Articles</Button>
           </div>
         </div>
-        <div>
-          <img src="/av.png" alt=""/>
+        <div className="flex items-center justify-center">
+          <img src="/av.png" className="drop-shadow-2xl h-auto" alt="" />
         </div>
       </div>
-      <div className="flex flex-col space-y-2">
-        <h1 className="text-4xl">Bruh</h1>
-        <h2 className="text-3xl">Bruh</h2>
-        <h3 className="text-2xl">Bruh</h3>
-        <p className="text-xl">Bruh</p>
+      <div className="flex items-center p-8 place-content-between">
+        <div>Scroll</div>
+        <div>
+          <Lottie
+            loop
+            animationData={animationData}
+            play
+            style={{ width: 300, height: 300 }}
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
