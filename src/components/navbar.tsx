2b9885a80d9fb7c922aa7cf8c6dfb3d92b1a5ee4
Tushar Manna <tushar.manna@outlook.com>
2025-04-20 00:41:18 +0530

    minor changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,10 +6,9 @@ export default function Navbar() {
         <div className="w-full h-[60px] border-b-[2.5px] border-primaryDark drop-shadow-2xl flex items-center justify-between px-6 bg-primary text-white fixed z-50">
             <Link className="text-2xl flex items-center" style={{ height: "inherit" }} href="/">
                 <img src="/22.png" alt="" style={{ height: "inherit" }} />
-                Bruh!
             </Link>
             <div className="flex space-x-6">
-                <Link href="/articles">Blog</Link>
+                <Link href="/articles">Articles</Link>
                 <Link href="/contact">Contact</Link>
             </div>
         </div>
